be99280d93164718dc793dbebea2452fe4bee8ec
Damian Albrich <damian.albrich@unitbv.ro>
2025-11-24 17:25:55 +0200

    properly made the project this time for intellij

--- CategorieProdus.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public enum CategorieProdus {
-    APERITIV,
-    FEL_PRINCIPAL,
-    DESERT,
-    BAUTURI_RACORITOARE,
-    BAUTURI_ALCOOLICE
-}
--- DiscountStrategy.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-@FunctionalInterface
-public interface DiscountStrategy {
-    double calculeazaReducere(Order order);
-}
